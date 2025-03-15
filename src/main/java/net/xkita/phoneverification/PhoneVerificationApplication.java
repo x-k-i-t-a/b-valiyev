343359ed1fc1f7b2b966436bca7fc3e0bdf07158
Nikita <rantewoo@gmail.com>
2025-03-15 16:08:21 +0300

    init commit

---
+++ src/main/java/net/xkita/phoneverification/PhoneVerificationApplication.java
@@ -0,0 +1,13 @@
+package net.xkita.phoneverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneVerificationApplication.class, args);
+    }
+
+}
